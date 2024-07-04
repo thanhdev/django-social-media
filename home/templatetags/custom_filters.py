2fdf7007a27d99b46ccda5f503a8970adbae8a23
tthanh <ytominhthanh@gmail.com>
2024-07-05 00:34:58 +0700

    post - Add YouTube thumbnails for YouTube links

--- home/templatetags/custom_filters.py
+++ home/templatetags/custom_filters.py
@@ -3,13 +3,31 @@ import re
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
 
+from home.utils import get_youtube_thumbnail
+
 register = template.Library()
+url_pattern = re.compile(
+    r"(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|%[0-9a-fA-F][0-9a-fA-F])+)"  # noqa
+)
 
 
 @register.filter
 def urlize(value):
-    url_pattern = re.compile(
-        r'(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)')
-    value = escape(value)  # Escape HTML characters
-    value = url_pattern.sub(r'<a href="\1" target="_blank">\1</a>', value)
+    # Escape HTML characters
+    value = escape(value)
+
+    # Replace other URLs with clickable links
+    value = url_pattern.sub(
+        r'<a class="text-blue-500" href="\1"' r' target="_blank">\1</a>', value
+    )
+
     return mark_safe(value)  # Mark the string as safe for HTML output
+
+
+@register.filter
+def youtube_thumbnail(value):
+    # Add YouTube thumbnail if the URL is a YouTube video
+    if thumbnail := get_youtube_thumbnail(value):
+        value += f'<br><img src="{thumbnail}" style="max-width: 100%;">'
+
+    return mark_safe(value)
